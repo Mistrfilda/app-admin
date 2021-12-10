8c13959b2d1dc0ba9944f196916f29787a20c50a
Filip Kuchar <filda.kuchar@seznam.cz>
2021-12-10 17:46:37 +0100

    Update icons enums

--- src/UI/Icon/SvgIcon.php
+++ src/UI/Icon/SvgIcon.php
@@ -6,9 +6,21 @@ namespace App\UI\Icon;
 
 class SvgIcon
 {
+	public const CHECK_CIRCLE = 'check-circle.svg';
+
+	public const CHEVRON_DOWN = 'chevron-down.svg';
+
+	public const EXCLAMATION = 'exclamation.svg';
 
 	public const HOME = 'home.svg';
 
+	public const INFORMATION_CIRCLE = 'information-circle.svg';
+
+	public const USER = 'user.svg';
+
 	public const USERS = 'users.svg';
 
+	public const X = 'x.svg';
+
+	public const X_CIRCLE = 'x-circle.svg';
 }
