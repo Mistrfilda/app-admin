da0292a20fd18bc59da300caf967aa32e402f723
Filip Kuchar <filda.kuchar@seznam.cz>
2021-12-10 19:23:39 +0100

    Enable purge in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,40 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {
-      colors: {
-        orange: colors.orange,
-        sky: colors.sky,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        indigo: colors.indigo
-      }
-    }
-  },
-  variants: {
-    extend: {
-      fontWeight: ["hover", "focus"]
-    }
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio")
-  ]
+	purge: {
+		content: [
+			'./src/**/*.html',
+			'./src/**/*.latte',
+			'./assets/**/*.html',
+			'./assets/**/*.latte',
+			'./assets/**/*.ts',
+			'./assets/**/*.js'
+		],
+		options: {
+			// Whitelisting some classes to avoid purge
+			safelist: [/^bg-/, /^text-/, /^border-/, /^hover-/]
+		}
+	},
+	theme: {
+		extend: {
+			colors: {
+				orange: colors.orange,
+				sky: colors.sky,
+				emerald: colors.emerald,
+				teal: colors.teal,
+				cyan: colors.cyan,
+				indigo: colors.indigo
+			}
+		}
+	},
+	variants: {
+		extend: {
+			fontWeight: ["hover", "focus"]
+		}
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/aspect-ratio")
+	]
 };
